94810180d7442a78d2e2d5c3df0e0fd0350488a0
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-16 12:25:24 -0500

    added

--- Portfolio2/script.js
+++ Portfolio2/script.js
@@ -21,6 +21,7 @@ document.addEventListener("click", (e) => {
     if(e.target.classList.contains("view-project-btn")) {
        // console.log("View");
        togglePortfolioPopup();
+       document.querySelector(".portfolio-popup").scrollTo(0,0);
        portfolioItemDetails(e.target.parentElement);
     }
 });
@@ -31,9 +32,17 @@ function togglePortfolioPopup(){
 }
 document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
 
+// hide popup when clicking outside of it
+document.addEventListener("click", (e) => {
+    //console.log(e.target);
+    if(e.target.classList.contains("pp-inner")){
+        togglePortfolioPopup();
+    }
+});
+
 function portfolioItemDetails(portfolioItem){
   //console.log(portfolioItem);
   document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
-  document.querySelector(".pp-header h3").innerHTML = portfolioItem.querySelector("portfolio-item-title").innerHTML;
+  document.querySelector(".pp-header h3").innerHTML = portfolioItem.querySelector(".portfolio-item-title").innerHTML;
   document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
 }
