e4b2b85c756f36b34b04f0a3e3c3b52e3ab321e3
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-16 12:07:01 -0500

    added

--- Portfolio2/script.js
+++ Portfolio2/script.js
@@ -14,3 +14,26 @@ tabContainer.addEventListener("click",(e) => {
     aboutSection.querySelector(target).classList.add("active");
    }
 });
+
+
+/* ------------------------- Portfolio Item Details Popup ---------------------------------- */
+document.addEventListener("click", (e) => {
+    if(e.target.classList.contains("view-project-btn")) {
+       // console.log("View");
+       togglePortfolioPopup();
+       portfolioItemDetails(e.target.parentElement);
+    }
+});
+function togglePortfolioPopup(){
+    document.querySelector(".portfolio-popup").classList.toggle("open");
+    document.body.classList.toggle("hide-scrolling");
+    document.querySelector(".main").classList.toggle("fade-out");
+}
+document.querySelector(".pp-close").addEventListener("click", togglePortfolioPopup);
+
+function portfolioItemDetails(portfolioItem){
+  //console.log(portfolioItem);
+  document.querySelector(".pp-thumbnail img").src = portfolioItem.querySelector(".portfolio-item-thumbnail img").src;
+  document.querySelector(".pp-header h3").innerHTML = portfolioItem.querySelector("portfolio-item-title").innerHTML;
+  document.querySelector(".pp-body").innerHTML = portfolioItem.querySelector(".portfolio-item-details").innerHTML;
+}
