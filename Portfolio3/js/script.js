c6fab8a5f5309cbdd7f754fa4403076e2cb91674
Rajia Rani <rajiarani.syal@gmail.com>
2024-03-20 12:24:17 -0500

    added

---
+++ Portfolio3/js/script.js
@@ -0,0 +1,7 @@
+/* ============= TYPING ANIMATION =============== */
+let typed = new Typed(".typing", {
+    strings : ["","web deginer", "Web Developer ", "Software Eng"],
+    typeSpeed : 100,
+    BackSpeed : 60,
+    loop : true,
+});
